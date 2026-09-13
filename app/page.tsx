a94545ccc097f4b13eb136ad21c8c613c5ed74ca
catstopia <catstopia@gmail.com>
2026-09-13 16:13:42 +0900

    Load managed front page settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,21 @@
-import { getPublishedPosts } from "./lib/content";
+import { getFrontSettings, getPublishedPosts } from "./lib/content";
 import { getChatGPTUser } from "./chatgpt-auth";
 import InteractiveFrontClient from "./ui/interactive-front-client";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const [notices, activities, user] = await Promise.all([
+  const [notices, activities, front, user] = await Promise.all([
     getPublishedPosts("notice", 5),
     getPublishedPosts("activity", 20),
+    getFrontSettings(),
     getChatGPTUser(),
   ]);
 
   return (
     <InteractiveFrontClient
       posts={[...notices, ...activities]}
+      front={front}
       showAdmin={Boolean(user)}
     />
   );
