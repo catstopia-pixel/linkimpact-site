6e69a19ec05e8b85ef184b839764a609a517ea4c
catstopia <catstopia@gmail.com>
2026-09-13 15:19:54 +0900

    Use interactive front page as site entry

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import { getPublishedPosts, getSettings } from "./lib/content";
+import { getPublishedPosts } from "./lib/content";
 import { getChatGPTUser } from "./chatgpt-auth";
-import HomeClient from "./ui/home-client";
+import InteractiveFrontClient from "./ui/interactive-front-client";
 
 export const dynamic = "force-dynamic";
+
 export default async function Home() {
-  const [settings, posts, user] = await Promise.all([getSettings(), getPublishedPosts(undefined, 12), getChatGPTUser()]);
-  return <HomeClient settings={settings} posts={posts} showAdmin={Boolean(user)} />;
+  const [posts, user] = await Promise.all([
+    getPublishedPosts(undefined, 12),
+    getChatGPTUser(),
+  ]);
+
+  return <InteractiveFrontClient posts={posts} showAdmin={Boolean(user)} />;
 }
